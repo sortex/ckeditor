1a91eda8ed325ca6f9edda85e211302d4fe366d8
wwalc <wwalc@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-12-17 00:16:06 +0000

    Updated the releases branch with CKEditor 3.5
    git-svn-id: http://svn.fckeditor.net/CKEditor/releases/stable@6244 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- ckeditor.asp
+++ ckeditor.asp
@@ -91,9 +91,9 @@ Class CKEditor
 
 
 	Private Sub Class_Initialize()
-		version = "3.4.3"
-		timeStamp = "ABH04T8"
-		mTimeStamp = "ABH04T8"
+		version = "3.5"
+		timeStamp = "ABH04T2"
+		mTimeStamp = "ABH04T2"
 
 		Set oInstanceConfig = CreateObject("Scripting.Dictionary")
 		Set oAllInstancesConfig = CreateObject("Scripting.Dictionary")
--- ckeditor_php5.php
+++ ckeditor_php5.php
@@ -20,11 +20,11 @@ class CKEditor
 	/**
 	 * The version of %CKEditor.
 	 */
-	const version = '3.4.3';
+	const version = '3.5';
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
-	const timestamp = 'ABH04T8';
+	const timestamp = 'ABH04T2';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	public $timestamp = "ABH04T8";
+	public $timestamp = "ABH04T2";
 	/**
 	 * An array that holds event listeners.
 	 */
